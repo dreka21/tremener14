066532bb13a39048a17eaf5455de886dd69b723c
dreka5 <razinboy@yandex.ru>
2024-06-08 10:25:24 +0300

    first commit

---
+++ git2/tremener14/tremener14/tremener14/Class1.cs
@@ -0,0 +1,13 @@
+﻿namespace tremener14
+{
+    public class Data14a
+    {
+        public int aa { get; set; }
+        internal int cc { get; set; }
+    }
+    public class Data14b
+    {
+        public int aab { get; set; }
+        internal int ccb { get; set; }
+    }
+}
